6feb15e8f57f05537f4ded8a8bb1d8faa38db4bd
thiemnn <thiemnn.nguyen@gmail.com>
2022-05-06 14:44:07 +0700

    1

--- NodeJs/sandi_server/src/services/products.service.js
+++ NodeJs/sandi_server/src/services/products.service.js
@@ -8,10 +8,11 @@ const get = async (_id) => {
         LEFT JOIN product_category B on A.id = B.product_id
         WHERE A.id = ` + _id)
         const images = await con.query("SELECT * FROM product_image where product_id = " + _id)
+        const attachments = await con.query("SELECT * FROM product_attachments where product_id = " + _id)
         const properties = await con.query("SELECT A.value, B.name as property_name, B.unit as property_unit FROM `product_properties` A LEFT join properties B on A.property_id = B.id where A.product_id = " + _id)
         await con.end()
         if(products && products.length > 0)
-            return {"product": products[0], "images": images, "properties": properties}
+            return {"product": products[0], "images": images, "properties": properties, "attachments": attachments}
         else 
             return null
     }
--- NodeJs/sandi_server/src/services/seo_urls.service.js
+++ NodeJs/sandi_server/src/services/seo_urls.service.js
@@ -4,7 +4,17 @@ const db = require('../db')
 const get = async (_id) =>{
     try {
         const con = await db.getConnection()
-        const seo_url = await con.query("SELECT * FROM url_seo_list where url_seo = '" + _id + "'")
+        const query = "select A.url_seo, A.url_type, A.relation_id, "+
+        "coalesce(B.meta_title, C.meta_title, D.meta_title, E.meta_title) as meta_title, "+
+        "coalesce(B.meta_desc, C.meta_desc, D.meta_desc, E.meta_desc) as meta_desc, "+
+        "coalesce(B.image, C.image, D.image, E.image) as image "+
+        "from url_seo_list as A " +
+        "left join categories as B on A.relation_id = B.id and A.url_type = 1 " +
+        "left join products as C on A.relation_id = C.id and A.url_type = 2 " +
+        "left join news_categories as D on A.relation_id = D.id and A.url_type = 3 " +
+        "left join news as E on A.relation_id = E.id and A.url_type = 4 " +
+        "where A.url_seo = '" + _id + "'";
+        const seo_url = await con.query(query)
         await con.end()
         return seo_url
     }
