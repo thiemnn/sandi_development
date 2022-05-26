6775e46449edc925e2dd011751201d995291a83e
thiemnn <thiemnn.nguyen@gmail.com>
2022-05-26 17:30:53 +0700

    1

---
+++ sandi_stock_reactjs/src/app/form-elements/Form1.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
+
+export class BasicElements extends Component {
+
+  state = {
+    startDate: new Date(),
+    testString: process.env.REACT_APP_TEST_STRING
+  };
+ 
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  componentDidMount() {
+    bsCustomFileInput.init()
+    this.fetchUsers()
+    //this.postUser()
+  }
+
+  async fetchUsers() {
+    try {
+      const response = await fetch("http://api.sandivietnam.com/products/209");
+      const data = await response.json();
+      return console.log(data);
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+
+  async postUser(){
+    const body = { 
+      news_id: 1,
+      name:'abc', 
+      email:'abc',
+      website:'abc',
+      comment:'abc',
+    }
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    };
+    const response = await fetch('http://api.sandivietnam.com/news/addComment', requestOptions);
+    const data = await response.json();
+    return console.log(data);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <h3 className="page-title"> Thêm mới nhà cung cấp {this.state.testString}</h3>          
+        </div>
+        <div className="row">
+          <div className="col-12 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <form className="forms-sample">
+                  <Form.Group>
+                    <label htmlFor="exampleInputName1">Name</label>
+                    <Form.Control type="text" className="form-control" id="exampleInputName1" placeholder="Name" />
+                  </Form.Group>
+                  <Form.Group>
+                    <label htmlFor="exampleInputEmail3">Email address</label>
+                    <Form.Control type="email" className="form-control" id="exampleInputEmail3" placeholder="Email" />
+                  </Form.Group>
+                  <Form.Group>
+                    <label htmlFor="exampleInputPassword4">Password</label>
+                    <Form.Control type="password" className="form-control" id="exampleInputPassword4" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group>
+                    <label htmlFor="exampleSelectGender">Gender</label>
+                    <select className="form-control" id="exampleSelectGender">
+                      <option>Male</option>
+                      <option>Female</option>
+                    </select>
+                  </Form.Group>
+                  <Form.Group>
+                    <label>File upload</label>
+                    <div className="custom-file">
+                      <Form.Control type="file" className="form-control visibility-hidden" id="customFileLang" lang="es"/>
+                      <label className="custom-file-label" htmlFor="customFileLang">Upload image</label>
+                    </div>
+                  </Form.Group>
+                  <Form.Group>
+                    <label htmlFor="exampleInputCity1">City</label>
+                    <Form.Control type="text" className="form-control" id="exampleInputCity1" placeholder="Location" />
+                  </Form.Group>
+                  <Form.Group>
+                    <label htmlFor="exampleTextarea1">Textarea</label>
+                    <textarea className="form-control" id="exampleTextarea1" rows="4"></textarea>
+                  </Form.Group>
+                  <button type="submit" className="btn btn-primary mr-2">Submit</button>
+                  <button className="btn btn-light">Cancel</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BasicElements
---
+++ sandi_stock_reactjs/src/app/form-elements/List.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import { ProgressBar } from 'react-bootstrap';
+
+export class BasicTable extends Component {
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <h3 className="page-title"> Danh sách nhà cung cấp </h3>          
+        </div>
+        <div className="row">
+          <div className="col-lg-12 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table className="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th> # </th>
+                        <th> First name </th>
+                        <th> Progress </th>
+                        <th> Amount </th>
+                        <th> Deadline </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td> 1 </td>
+                        <td> Herman Beck </td>
+                        <td>
+                          <ProgressBar variant="success" now={25} />
+                        </td>
+                        <td> $ 77.99 </td>
+                        <td> May 15, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td> 2 </td>
+                        <td> Messsy Adam </td>
+                        <td>
+                          <ProgressBar variant="danger" now={75} />
+                        </td>
+                        <td> $245.30 </td>
+                        <td> July 1, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td> 3 </td>
+                        <td> John Richards </td>
+                        <td>
+                          <ProgressBar variant="warning" now={90} />
+                        </td>
+                        <td> $138.00 </td>
+                        <td> Apr 12, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td> 4 </td>
+                        <td> Peter Meggik </td>
+                        <td>
+                          <ProgressBar variant="primary" now={50} />
+                        </td>
+                        <td> $ 77.99 </td>
+                        <td> May 15, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td> 5 </td>
+                        <td> Edward </td>
+                        <td>
+                          <ProgressBar variant="danger" now={35} />
+                        </td>
+                        <td> $ 160.25 </td>
+                        <td> May 03, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td> 6 </td>
+                        <td> John Doe </td>
+                        <td>
+                          <ProgressBar variant="info" now={65} />
+                        </td>
+                        <td> $ 123.21 </td>
+                        <td> April 05, 2015 </td>
+                      </tr>
+                      <tr>
+                        <td> 7 </td>
+                        <td> Henry Tom </td>
+                        <td>
+                        <ProgressBar now={60} />
+                          <ProgressBar variant="warning" now={20} />
+                        </td>
+                        <td> $ 150.00 </td>
+                        <td> June 16, 2015 </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BasicTable
